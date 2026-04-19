020ad6c7480c76ac260527675a51b50fa771c797
Mustafa <mustafadogan@piton.com.tr>
2026-04-19 11:20:36 +0300

    feat(exceptions): add base AuthForgeException and ErrorCodes constants [AF-5]

---
+++ src/AuthForge/Constants/FeatureConstants.cs
@@ -0,0 +1,9 @@
+namespace AuthForge.Exceptions;
+
+/// <summary>
+/// Tüm modül özelliklerini tanımlayan sabitler
+/// </summary>
+public static class FeatureConstants
+{
+    public const string AuthForgeFeatureName = "AuthForge";
+}
